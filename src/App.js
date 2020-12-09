00c28d4dc1f418f6b96784c02b4f2a0a4a47ebf3
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-09 15:49:47 +0100

    Create game and sidebar empty components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Game from "./components/Game";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <div className="App">
+      <Game/>
+      <SideBar/>
     </div>
   );
 }
