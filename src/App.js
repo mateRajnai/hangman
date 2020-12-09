0c930af7cdab6ae6a7d83e0ce702c42f7ed8c778
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-09 16:01:41 +0100

    display flex app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Game from "./components/Game";
 import SideBar from "./components/SideBar";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
+    <StyleWrapper id="App">
       <Game/>
       <SideBar/>
-    </div>
+    </StyleWrapper>
   );
 }
 
 export default App;
+
+const StyleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
