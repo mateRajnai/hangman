9122d2682ef9cf745276a6afc04319dba1c06300
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-10 10:48:23 +0100

    Create empty VocabularyContext

---
+++ src/context/VocabularyContext.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const VocabularyContext = React.createContext();
+
+export const VocabularyProvider = (props) => {
+
+
+    return (
+        <VocabularyContext.Provider value={{
+
+        }}>
+            {props.children}
+        </VocabularyContext.Provider>
+    );
+};
+
