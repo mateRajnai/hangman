be9b69aece6601a2631da1864c7ef0e6a01a53c7
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-10 10:52:10 +0100

    Add VocabularyProvider to the component tree

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Game from "./components/Game";
 import SideBar from "./components/SideBar";
 import styled from "styled-components";
+import {VocabularyProvider} from "./context/VocabularyContext"
 
 function App() {
   return (
     <StyleWrapper id="App">
-      <Game/>
-      <SideBar/>
+      <VocabularyProvider>
+        <Game/>
+        <SideBar/>
+      </VocabularyProvider>
     </StyleWrapper>
   );
 }
--- src/context/VocabularyContext.js
+++ src/context/VocabularyContext.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export const VocabularyContext = React.createContext();
 
 export const VocabularyProvider = (props) => {
 
 
+  
+
+
     return (
         <VocabularyContext.Provider value={{
-
+           
         }}>
             {props.children}
         </VocabularyContext.Provider>
