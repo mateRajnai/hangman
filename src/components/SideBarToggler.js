0843b608ed211eb39ee47e5757c29a11edef46fd
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-10 10:21:33 +0100

    Add showVocabularies button

--- src/components/SideBarToggler.js
+++ src/components/SideBarToggler.js
@@ -2,11 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 const SideBarToggler = () => {
-    return (<StyleWrapper id="side-bar-toggler" className="styled-div"></StyleWrapper>)
+
+    const showVocabularies = () => {
+        
+    }
+
+    return (
+    <StyleWrapper id="side-bar-toggler" className="styled-div">
+        <button type="button" onclick={showVocabularies}>Change vocabulary</button>
+    </StyleWrapper>
+    )
 }
 
 export default SideBarToggler;
 
 const StyleWrapper = styled.div`
     flex: 1;
+
+    button {
+        width: 25%;
+        height: 70%;
+    }
 `;
