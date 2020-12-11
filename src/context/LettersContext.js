f399aed8b3640bf45cad1cf7d1de8c93fed9653e
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-11 09:13:50 +0100

    Get generatedWord and create correctLetters vars in LettersContext

--- src/context/LettersContext.js
+++ src/context/LettersContext.js
@@ -1,10 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {VocabularyContext} from './VocabularyContext';
 
 export const LettersContext = React.createContext();
 
 export const LettersProvider = (props) => {
 
+    const {generatedWord} = useContext(VocabularyContext);
+
     const [guessedLetter, setGuessedLetter] = useState("");
+    const [correctLetters, setCorrectLetters] = useState([]);
 
 
     useEffect(() => {
