68b99ceab763ef67dc97622c105eae80d12f98d8
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-14 14:36:34 +0100

    create structure for alerts

--- src/components/GameEndingAlert.js
+++ src/components/GameEndingAlert.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
+import { GameContext } from '../context/GameContext';
+import styled from 'styled-components';
 
 const GameEndingAlert = () => {
 
+
+    const {isEndOfGame, isPlayerWon} = useContext(GameContext);
+
+    useEffect(() => {
+        console.log(isPlayerWon)
+    }, [isPlayerWon])
+
     return (
-        <div></div>
+        <StyleWrapper id="game-ending-alert">
+            {isEndOfGame ? 
+                isPlayerWon ?
+                    <div className="alert alert-success" role="alert">
+                        <strong>Well done!</strong> You successfully read this
+                        important alert message.
+                    </div>
+                    :
+                    <div className="alert alert-danger" role="alert">
+                        <strong>Oh snap!</strong> Change a few things up and try submitting again.
+                    </div>
+                :
+                <div></div>
+            }
+        </StyleWrapper>
     );
 };
 
 
 export default GameEndingAlert;
+
+const StyleWrapper = styled.div`
+    height: 150px;
+    width: 150px;
+
+    .alert {
+        height: 150px;
+        width: 150px;
+    }
+`;
