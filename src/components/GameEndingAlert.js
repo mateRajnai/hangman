73932b1984237b8f51d6dc16796dfea4fc80a15d
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-14 15:29:12 +0100

    Center game ending alert verticallay and horizontaly

--- src/components/GameEndingAlert.js
+++ src/components/GameEndingAlert.js
@@ -8,23 +8,27 @@ const GameEndingAlert = () => {
     const {isEndOfGame, isPlayerWon} = useContext(GameContext);
 
     useEffect(() => {
-        console.log(isPlayerWon)
+        console.log(isPlayerWon);
     }, [isPlayerWon])
 
     return (
         <StyleWrapper id="game-ending-alert">
-            {isEndOfGame ? 
-                isPlayerWon ?
-                    <div className="alert alert-success" role="alert">
-                        <strong>Well done!</strong> You successfully read this
-                        important alert message.
-                    </div>
-                    :
-                    <div className="alert alert-danger" role="alert">
-                        <strong>Oh snap!</strong> Change a few things up and try submitting again.
-                    </div>
-                :
+            {isEndOfGame ? (
+                isPlayerWon ? (
+                        <div className="alert alert-success" role="alert">
+                            <strong>Well done!</strong> You successfully read this
+                            important alert message.
+                        </div>
+                    )
+                    : (
+                        <div className="alert alert-danger" role="alert">
+                            <strong>Oh snap!</strong> Change a few things up and try submitting again.
+                        </div>
+                    )
+                )
+                : (
                 <div></div>
+                )
             }
         </StyleWrapper>
     );
@@ -34,8 +38,13 @@ const GameEndingAlert = () => {
 export default GameEndingAlert;
 
 const StyleWrapper = styled.div`
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
     height: 150px;
     width: 150px;
+    z-index: 100;
 
     .alert {
         height: 150px;
