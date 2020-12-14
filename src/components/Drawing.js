933896ad9ff11ab685d6115303d02f157fcd8206
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-14 10:19:55 +0100

    Change html element order of drawing

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -14,6 +14,12 @@ const Drawing = () => {
 
     return <StyleWrapper id="drawing" className="styled-div">
                 <svg height="250" width="100%">
+                    <g id="gallows">
+                        <line x1="10" y1="230" x2="270" y2="230" />
+                        <line x1="100" y1="230" x2="100" y2="20" />
+                        <line x1="100" y1="20" x2="200" y2="20" />
+                        <line id="rope" x1="200" y1="20" x2="200" y2="60" />
+                    </g>
                     <g id="body">
                         <circle cx="200" cy="80" r="20" stroke="white" strokeWidth="4" fill="white"/>
                         <line x1="200" y1="100" x2="200" y2="150" />
@@ -21,12 +27,6 @@ const Drawing = () => {
                         <line id="armR" x1="200" y1="120" x2="230" y2="140" />
                         <line id="legL" x1="200" y1="150" x2="180" y2="190" />
                         <line id="legR" x1="200" y1="150" x2="220" y2="190" />
-                    </g>
-                    <g id="gallows">
-                        <line x1="10" y1="230" x2="270" y2="230" />
-                        <line x1="100" y1="230" x2="100" y2="20" />
-                        <line x1="100" y1="20" x2="200" y2="20" />
-                        <line id="rope" x1="200" y1="20" x2="200" y2="60" />
                     </g>
                 </svg>
     </StyleWrapper>
