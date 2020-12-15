0a72d6c84fb2d2fd453a92b0fb6a50269b3fdf14
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-15 10:25:28 +0100

    Invoke generateWord in startNewGame

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
+import {VocabularyContext} from './VocabularyContext';
 
 export const GameContext = React.createContext();
 
@@ -6,10 +7,12 @@ export const GameProvider = (props) => {
 
     const [isEndOfGame, setIsEndOfGame] = useState(false);
     const [isPlayerWon, setIsPlayerWon] = useState(false);
+    const {generateWord} = useContext(VocabularyContext);
 
-    const startNewGame = () => {
+    const startNewGame = (e) => {
         setIsEndOfGame(false);
         setIsPlayerWon(false);
+        generateWord(e);
     }
 
     return (
