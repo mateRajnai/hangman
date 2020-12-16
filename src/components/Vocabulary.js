d8d0c3153cab3afc10fb12d4d02e24dd2116ed73
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-16 13:23:09 +0100

    Add flex, color, with to Vocabulary.js

--- src/components/Vocabulary.js
+++ src/components/Vocabulary.js
@@ -10,14 +10,15 @@ const Vocabulary = () => {
     const {generateWord} = useContext(VocabularyContext);
 
     return (
-        <div>
+        <StyleWrapper>
+            <h1>Vocabularies</h1>
             <button type="button" className="flag-button"  onClick={generateWord}>
                 <StyledFlag src={englishFlag} alt="English vocabulary" data-vocabulary="english"></StyledFlag>
             </button>
             <button type="button" className="flag-button" onClick={generateWord}>
                 <StyledFlag src={germanFlag} alt="German vocabulary" data-vocabulary="german"></StyledFlag>
             </button>
-        </div>
+        </StyleWrapper>
     );
 };
 
@@ -29,4 +30,16 @@ const StyledFlag = styled.img`
     height: 80px;
     cursor: pointer;
     border: 0
+`;
+
+const StyleWrapper = styled.div`
+    width: 100%;
+    background-color: #383961;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+        margin-top: 5%;
+    }
 `;
