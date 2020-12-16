2e207227a2019b43b73fd55be013b581a8e18f27
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-16 11:34:20 +0100

    Add button and StyledFlag as content to Vocabulary

--- src/components/Vocabulary.js
+++ src/components/Vocabulary.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {VocabularyContext} from '../context/VocabularyContext';
+import englishFlag from '../images/english_flag.png';
+import germanFlag from '../images/german_flag.png';
+import styled from 'styled-components';
+
 
 const Vocabulary = () => {
 
+    const {generateWord} = useContext(VocabularyContext);
 
     return (
-        <div></div>
+        <div>
+            <button type="button" className="flag-button"  onClick={generateWord}>
+                <StyledFlag src={englishFlag} alt="English vocabulary" data-vocabulary="english"></StyledFlag>
+            </button>
+            <button type="button" className="flag-button" onClick={generateWord}>
+                <StyledFlag src={germanFlag} alt="German vocabulary" data-vocabulary="german"></StyledFlag>
+            </button>
+        </div>
     );
 };
 
 export default Vocabulary;
+
+
+const StyledFlag = styled.img`
+    width: 160px;
+    height: 80px;
+    cursor: pointer;
+    border: 0
+`;
