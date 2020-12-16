344d5154b1dfaa6885f10dbef9d154f6787dc1b0
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-16 14:54:33 +0100

    Remove console log and imports

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useContext} from 'react';
+import React, {useState, useContext} from 'react';
 import {VocabularyContext} from './VocabularyContext';
 
 export const GameContext = React.createContext();
@@ -13,6 +13,8 @@ export const GameProvider = (props) => {
         setIsEndOfGame(false);
         setIsPlayerWon(false);
         generateWord(e);
+        console.log(isPlayerWon)
+        console.log(isEndOfGame)
     }
 
 
