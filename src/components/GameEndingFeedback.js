93c5224b55dde687580035e993ed1aac6df37196
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-17 11:23:17 +0100

    Refactor GameEndingFeedback

--- src/components/GameEndingFeedback.js
+++ src/components/GameEndingFeedback.js
@@ -4,27 +4,20 @@ import styled from 'styled-components';
 
 const GameEndingFeedback = () => {
 
-
     const {isEndOfGame, isPlayerWon, startNewGame} = useContext(GameContext);
 
     return (
+        isEndOfGame &&
         <StyleWrapper id="game-ending-alert">
-            {isEndOfGame && isPlayerWon &&
-                        <div className="alert alert-success" role="alert">
-                            <strong>Well done! You won the game.</strong> 
-                            <button onClick={e => startNewGame(e)} className="new-game-button">Start new game</button>
-                        </div>
-            }
-            {isEndOfGame && !isPlayerWon &&
-                        <div className="alert alert-danger" role="alert">
-                            <strong>Oh snap! Game over.</strong>
-                            <button onClick={e => startNewGame(e)} className="new-game-button">Start new game</button>
-                        </div>
-            }
+                            <strong>{isPlayerWon ? 
+                                "Well done! You won the game." : 
+                                "Oh snap! Game over."}</strong> 
+                            <button onClick={e => startNewGame(e)} className="new-game-button">
+                                Start new game
+                            </button>
         </StyleWrapper>
     );
 };
-
 
 export default GameEndingFeedback;
 
@@ -34,16 +27,13 @@ const StyleWrapper = styled.div`
     top: 50%;
     transform: translate(-50%, -50%);
     z-index: 100;
-
-    .alert {
-        height: 150px;
-        width: 200px;
-        background-color: #503047;
-        padding: 20px 20px;
-        border: 3px solid;
-        border-radius: 20px;
-        border-color: white;
-    }
+    height: 150px;
+    width: 200px;
+    background-color: #503047;
+    padding: 20px 20px;
+    border: 3px solid;
+    border-radius: 20px;
+    border-color: white;
 
     .new-game-button {
         margin-top: 5px;
