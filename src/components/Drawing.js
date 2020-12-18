76d0d52f8385a1af34b179cd79ff74b7396822fc
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-18 10:24:13 +0100

    Uncomment hangman drawing

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -37,17 +37,17 @@ const Drawing = () => {
                     <g id="gallows">
                         <line x1="10" y1="230" x2="270" y2="230" className="drawing-part"/>
                         <line x1="100" y1="230" x2="100" y2="20" className="drawing-part"/>
-                        {/* <line x1="100" y1="20" x2="200" y2="20" className="drawing-part"/>
-                        <line id="rope" x1="200" y1="20" x2="200" y2="60" className="drawing-part"/> */}
+                        <line x1="100" y1="20" x2="200" y2="20" className="drawing-part"/>
+                        <line id="rope" x1="200" y1="20" x2="200" y2="60" className="drawing-part"/>
                     </g>
-                    {/* <g id="body">
+                    <g id="body">
                         <circle cx="200" cy="80" r="20" stroke="white" strokeWidth="4" fill="white" className="drawing-part"/>
                         <line x1="200" y1="100" x2="200" y2="150" className="drawing-part"/>
                         <line id="armL" x1="200" y1="120" x2="170" y2="140" className="drawing-part"/>
                         <line id="armR" x1="200" y1="120" x2="230" y2="140" className="drawing-part"/>
                         <line id="legL" x1="200" y1="150" x2="180" y2="190" className="drawing-part"/>
                         <line id="legR" x1="200" y1="150" x2="220" y2="190" className="drawing-part"/>
-                    </g> */}
+                    </g>
                 </svg>
     </StyleWrapper>
 }
