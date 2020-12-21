23497a680dd79333efabda186a5da5b1a67bcddd
Rajnai Máté <ramate.rajnai@gmail.com>
2020-12-21 09:56:11 +0100

    Set font size for correct letters

--- src/components/CorrectLetter.js
+++ src/components/CorrectLetter.js
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 
 const CorrectLetter = (props) => {
     return (
-        <StyleWrapper className="correct-letter">
-            {props.letter}
+        <StyleWrapper className="correct-letter-container">
+            <div className="correct-letter">
+                {props.letter}
+            </div>
         </StyleWrapper>
     );
 };
@@ -13,9 +15,14 @@ export default CorrectLetter;
 
 
 const StyleWrapper = styled.div`
-    height: 15%;
+    height: 25%;
     width: 4%;
-    margin: 8px;
-    color: white;   
     border-bottom: 5px solid white;
+    margin: 8px;
+
+    .correct-letter {
+        font-size: 150%;
+        width: 40%;
+        color: white;   
+    }
 `;
